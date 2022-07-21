97b1a8a7c5e89aec0b100bf647a2a33fc58c3eda
BhumikaSaini1999 <sainibhumika1624@gmail.com>
2022-07-21 22:23:50 +0530

    code added

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,95 @@
+package Sorting;
+
+import java.util.Scanner;
+
+//Time=>O(nlogn)
+//Space=>O(n)
+public class MergeSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    Scanner sc=new Scanner(System.in);
+			
+			System.out.println("Enter size of array:");
+			int n=sc.nextInt();
+			
+			int arr[]=new int[n];
+			System.out.println("Enter array elements:");
+			for(int i=0;i<n;i++)
+			{
+				arr[i]=sc.nextInt();
+			}
+			
+			mergeSort(arr,0,n-1);
+			System.out.println("The sorted Array is: ");
+			for(int i:arr)
+				System.out.print(i+" ");
+
+	}
+
+	private static void mergeSort(int[] arr, int l, int h) {
+		// TODO Auto-generated method stub
+		if(l<h)
+		{
+			int mid=(l+h)/2;
+			mergeSort(arr,l,mid);
+			mergeSort(arr,mid+1,h);
+			merge(arr,l,mid,h);
+		}	
+	}
+
+	private static void merge(int[] arr, int l, int mid, int h) {
+		// TODO Auto-generated method stub
+		
+		int i=l;
+		int j=mid+1;
+		int k=0;
+		
+		int b[]=new int[h-l+1];
+		
+		while(i<=mid && j<=h) //Comparing the elements of two divided arrays
+		{
+			if(arr[i]<=arr[j])
+			{
+				b[k]=arr[i];
+				i++;
+				k++;
+			}
+			else
+			{
+				b[k]=arr[j];
+				j++;
+				k++;
+			}
+		}
+		
+		if(i<=mid) //copying the remaining elements back to b[] as it is, if still i<=mid
+		{
+			while(i<=mid)
+			{
+				b[k]=arr[i];
+				i++;
+				k++;
+			}
+		}
+		
+		if(j<=h) //copying the remaining elements back to b[] as it is, if still j<=h
+		{
+			while(j<=h)
+			{
+				b[k]=arr[j];
+				j++;
+				k++;
+			}
+		}
+		
+		//copy b[] to arr[]=> As b[] is used for only for merging purpose
+		
+		for(i=l,k=0;k<b.length;k++)
+		{
+			arr[l++]=b[k];
+		}
+		
+	}
+
+}
